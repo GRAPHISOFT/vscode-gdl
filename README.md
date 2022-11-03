9548a880199b0bcfcfb4f5b12e4ed29e250a0081
pbaksa <pbaksa@graphisoft.com>
2022-11-03 13:09:05 +0100

    Revert "use HSFLibpart to handle other scripts of libpart"
    This reverts commit 32463ecd9ae68275419fc8b669fb5601e4c7ab1d.

--- README.md
+++ README.md
@@ -49,11 +49,12 @@ __hotspot__ or __hotspot2__ - length edit hotspot triplet
   * Select to open the same script or xml that the current editor contains, or libpartdata.xml if unknown
   * If the workspace changed in a process outside VSCode, use the "Re-scan library parts in workspace folders" command to refresh the known library parts.
 * When a folder of HSF files is opened, further features are available:
-  * Show info about parameters when the mouse hovers on them (can be toggled on/off with Ctrl-Alt-Space)
-  * Show parameters with bold font (can be toggled on/off with Ctrl-Alt-Space)
+  * Show info about parameters when the mouse hovers on them
+  * Show parameters with bold font
   * IntelliSense lists constants (ALL_CAPS) with their initialized values
   * IntelliSense lists parameters with their description, type, flags and default value. This list can be filtered by parameter type too.
-  * Follow macro and subroutine calls with Ctrl-click
+  * These can all be toggled on/off together with Ctrl-Shift-Space
+  * Follow macro calls with Ctrl-click
 * Go to definitions/references, find all references of subroutines in same libpart (Ctrl-click or F12, Shift-F12, Shift-Alt-F12)
 * Show Call Hierarchy (Shift-Alt-H), Peek Call Hierarchy
   * Incoming or outgoing macro calls are shown, respecting the execution context of the edited scipt. Eg. all scripts are searched outgoing from a master script, but only master and 2d scripts are searched outgoing from a 2d script.
@@ -170,7 +171,6 @@ Some valid syntaxes won't be highlighted, and some invalid syntaxes will be high
 
 ## 1.26.2
 * Intellisense reads constants from edited file too, besides master script
-* IntelliSense parameters and constants can no longer be toggled with Ctrl-Alt-Space, use vscode settings to filter the results (`editor.suggest.showFields`, `editor.suggest.showConstants`)
 * Find references/definition handles subroutines defined in master script
 
 ## 1.26.1
--- out/parsehsf.js
+++ out/parsehsf.js
@@ -2,38 +2,26 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.HSFLibpart = void 0;
 const vscode = require("vscode");
-const Parser = require("./parsexmlgdl");
 const paramlistparser_1 = require("./paramlistparser");
 const constparser_1 = require("./constparser");
 const wssymbols_1 = require("./wssymbols");
 class HSFLibpart {
-    constructor(rootFolder, currentScript) {
+    constructor(rootFolder) {
         this._paramlist = new paramlistparser_1.ParamList();
-        this._constants = new Map();
-        this.info = new wssymbols_1.LibpartInfo(vscode.Uri.joinPath(rootFolder, "libpartdata.xml"), "");
+        this._masterconstants = new constparser_1.Constants();
         this.processing = Promise.allSettled([
-            this.constants(Parser.ScriptType.D),
-            this.constants(currentScript),
+            this.read_master_constants(),
             this.read_paramlist()
         ]);
-        //TODO register paramlist observer
+        this.info = new wssymbols_1.LibpartInfo(vscode.Uri.joinPath(rootFolder, "libpartdata.xml"), "");
     }
     get paramlist() { return this._paramlist; }
-    async refresh(script) {
-        this._constants.delete(script);
-        await this.constants(script);
-    }
+    get masterconstants() { return this._masterconstants; }
     async read_paramlist() {
         await this._paramlist.addfrom(this.info.root_uri);
     }
-    async constants(script) {
-        let constants = this._constants.get(script);
-        if (constants === undefined) {
-            constants = new constparser_1.Constants();
-            await constants.addfromfile(this.info.root_uri, `scripts/${Parser.scriptFile[script]}.gdl`);
-            this._constants.set(script, constants);
-        }
-        return constants;
+    async read_master_constants() {
+        await this._masterconstants.addfromfile(this.info.root_uri, "scripts/1d.gdl");
     }
 }
 exports.HSFLibpart = HSFLibpart;
