32463ecd9ae68275419fc8b669fb5601e4c7ab1d
pbaksa <pbaksa@graphisoft.com>
2022-11-03 12:35:57 +0100

    use HSFLibpart to handle other scripts of libpart don't use extension setting for intellisense filtering

--- src/parsehsf.ts
+++ src/parsehsf.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 
+import * as Parser from './parsexmlgdl';
 import { ParamList } from './paramlistparser';
 import { Constants } from './constparser';
 import { LibpartInfo } from './wssymbols';
@@ -8,26 +9,39 @@ export class HSFLibpart {
     private readonly _paramlist: ParamList = new ParamList();
     get paramlist() : ParamList { return this._paramlist; }
 
-    private readonly _masterconstants: Constants = new Constants();
-    get masterconstants() : Constants { return this._masterconstants; }
+    private readonly _constants = new Map<Parser.ScriptType, Constants>();
 
     readonly processing : Promise<PromiseSettledResult<void>[]>;
     public readonly info : LibpartInfo;
 
-    constructor(rootFolder : vscode.Uri) {
+    constructor(rootFolder : vscode.Uri, currentScript : Parser.ScriptType) {
+        this.info = new LibpartInfo(vscode.Uri.joinPath(rootFolder, "libpartdata.xml"), ""); 
+
         this.processing = Promise.allSettled([  // parallel execution
-            this.read_master_constants(),
+            this.constants(Parser.ScriptType.D) as unknown as Promise<void>,
+            this.constants(currentScript) as unknown as Promise<void>,
             this.read_paramlist()
         ]);
 
-        this.info = new LibpartInfo(vscode.Uri.joinPath(rootFolder, "libpartdata.xml"), ""); 
+        //TODO register paramlist observer
+    }
+
+    public async refresh(script: Parser.ScriptType) {
+        this._constants.delete(script);
+        await this.constants(script);
     }
 
     private async read_paramlist() {
         await this._paramlist.addfrom(this.info.root_uri);
     }
 
-    private async read_master_constants() {
-        await this._masterconstants.addfromfile(this.info.root_uri, "scripts/1d.gdl");
+    public async constants(script: Parser.ScriptType) : Promise<Constants> {
+        let constants = this._constants.get(script);
+        if (constants === undefined) {
+            constants = new Constants();
+            await constants.addfromfile(this.info.root_uri, `scripts/${Parser.scriptFile[script]}.gdl`)
+            this._constants.set(script, constants);
+        }
+        return constants;
     }
 }
